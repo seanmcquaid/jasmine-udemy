2b78f4201a9a6e4e763be36f9005e4581cd1b401
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-18 23:19:17 -0400

    created calculator class for operations

---
+++ simple-calculator/calculator.js
@@ -0,0 +1,22 @@
+function Calculator (){
+    this.total = 0;
+}
+
+Calculator.prototype.add = function(number){
+    return this.total += number;
+}
+
+Calculator.prototype.subtract = function(number){
+    return this.total -= number;
+}
+
+Calculator.prototype.multiply = function(number){
+    return this.total *= number;
+}
+
+Calculator.prototype.divide = function(number){
+    if(number === 0){
+        throw new Error("cannot divide by 0")
+    }
+    return this.total /= number;
+}
---
+++ simple-calculator/main.js
@@ -0,0 +1,6 @@
+function calculate(inputValue){
+    const numberA;
+    const numberB;
+
+    const operation;
+}
