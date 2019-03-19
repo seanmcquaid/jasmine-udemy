29dbabf3e9607df097c10882078df9bd49576511
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-19 17:30:20 -0400

    basic calc functionality done

--- simple-calculator/main.js
+++ simple-calculator/main.js
@@ -1,6 +1,38 @@
 function calculate(inputValue){
-    const numberA;
-    const numberB;
+    const expression = /\+|\-|\*|\//;
+    const numbers = inputValue.split(expression);
+    // debugger
+    const numberA = parseInt(numbers[0]);
+    const numberB = parseInt(numbers[1]);
 
-    const operation;
+    const operation = inputValue.match(expression)
+    // debugger
+
+    const calculator = new Calculator();
+    calculator.add(numberA);
+
+    let result;
+    switch(operation[0]){
+        case "+":
+            result = calculator.add(numberB);
+            break
+        case "-":
+            result = calculator.subtract(numberB);
+            break
+        case "*":
+            result = calculator.multiply(numberB);
+            break
+        case "/":
+            result = calculator.divide(numberB);
+            break
+    }
+
+    updateResult(result);
+}
+
+function updateResult(result){
+    const element = document.getElementById("result");
+    if(element){
+        element.innerText = result;
+    }
 }
