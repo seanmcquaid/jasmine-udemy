cc27b4ee4161d428af29995952f815adf60d94ac
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-19 18:50:26 -0400

    first suite built

---
+++ simple-calculator/calculator.spec.js
@@ -0,0 +1,10 @@
+// suite = collection of specs
+// allows us to organize our specs
+
+// describe method is native and takes two args: 
+// 1. a string to describe the suite and show in the report
+// 2. an anonymous function for our tests
+
+describe("calculator.js", function(){
+    // put specs here
+});
