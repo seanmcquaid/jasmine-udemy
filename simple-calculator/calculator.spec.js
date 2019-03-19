fa33770b213d837888631149cfd4df4478e6a6f0
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-19 19:48:39 -0400

    wrote my first tests

--- simple-calculator/calculator.spec.js
+++ simple-calculator/calculator.spec.js
@@ -5,6 +5,23 @@
 // 1. a string to describe the suite and show in the report
 // 2. an anonymous function for our tests
 
+// expecation = assertion/statement
+// true / false value returns
+
+// 5 + 5 = 10 - statement
+
+// expect 5 + 5 to be 10 
+// result is true
+// expect 5 + 4 to be 10 
+// result is false
+
+
+// passing and failing specs
+
+// specs with all true expecations = passing
+
+// specs with one of more false expectations = failing spec
+
 describe("calculator.js", function(){
     // put specs here
     // it("title of the spec", function(){
@@ -12,18 +29,32 @@ describe("calculator.js", function(){
     // })
 
     it("should add numbers from total", function(){
-        // todo : EXPECTATIONS
+        const calculator = new Calculator();
+        calculator.add(5);
+        // expect total to be 5
+        // put actual value in expect
+        // to be is expected value
+        expect(calculator.total).toBe(5);
     })
 
     it("should subtract numbers from total", function(){
-        // todo : EXPECTATIONS
+        const calculator = new Calculator();
+        calculator.total = 30;
+        calculator.subtract(5);
+        expect(calculator.total).toBe(25);
     })
 
     it("should multiply total by number", function(){
-        // todo : EXPECTATIONS
+        const calculator = new Calculator();
+        calculator.total = 100;
+        calculator.multiply(2);
+        expect(calculator.total).toBe(200);
     })
 
     it("should divide total by number", function(){
-        // todo : EXPECTATIONS
+        const calculator = new Calculator();
+        calculator.total = 200;
+        calculator.divide(2);
+        expect(calculator.total).toBe(100);
     })
 });
