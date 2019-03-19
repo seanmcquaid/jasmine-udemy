83c4ce4e9bee66626230f27a8a41c66a63588bc8
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-19 19:55:42 -0400

    learned about disabling specs/suites

--- simple-calculator/calculator.spec.js
+++ simple-calculator/calculator.spec.js
@@ -22,7 +22,21 @@
 
 // specs with one of more false expectations = failing spec
 
-describe("calculator.js", function(){
+// disabled spec = spec that reports as pending and will be executed
+
+// 2 common cases! 
+
+// 1. changes in the code
+// 2. TDD > Test Driven Development
+
+// disable by typing x in front of it for spec
+
+// disabled suite :
+// all specs within disabled suite will be marked as pending and not executed
+
+// xdescribe works just like xit
+
+xdescribe("calculator.js", function(){
     // put specs here
     // it("title of the spec", function(){
     //     // todo : EXPECTATIONS
