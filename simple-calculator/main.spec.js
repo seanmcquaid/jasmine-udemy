2e96593e8a8d3ad85e8f0f1d8f75fa08af95e35a
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-22 18:02:19 -0400

    learned before all and after all

---
+++ simple-calculator/main.spec.js
@@ -0,0 +1,29 @@
+describe("main.js", function(){
+    describe("calculate()", function(){
+        xit("validates expression");
+        xit("calls add");
+        xit("calls subtract");
+        xit("calls multiply");
+        xit("calls divide");
+        xit("validates operation");
+        xit("calls updateResult");
+    });
+    describe("updateResult()", function(){
+        let element;
+        beforeAll(function(){
+            // executed once before all the specs are executed
+            element = document.createElement("div");
+            element.setAttribute("id", "result");
+            document.body.appendChild(element);
+        })
+        afterAll(function(){
+            const element = document.getElementById("result");
+            document.body.removeChild(element);
+        });
+
+        it("adds result to DOM element", function(){
+            updateResult("5");
+            expect(element.innerText).toBe("5");
+        })
+    });
+})
