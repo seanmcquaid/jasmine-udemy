9e092b3fe83e8e8b6fd566c7984b1acb8b8e7f40
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-23 15:42:15 -0400

    learned about toHaveBeenCalled and variations

--- simple-calculator/main.spec.js
+++ simple-calculator/main.spec.js
@@ -2,27 +2,35 @@ describe("main.js", function(){
     describe("calculate()", function(){
         it("validates expression when the first number is invalid", function(){
             spyOn(window, "updateResult").and.stub();
-
+            // and stub is default to spyOn but can call to stop the call after
             calculate("a+3");
             // our spy was installed on window.updateResult so we have to expect that
             // checking to see if this has been called at least once when calculate has been executed
-            expect(window.updateResult).toHaveBeenCalled()
+            expect(window.updateResult).toHaveBeenCalled();
+            // called with checks the arguments passed in
+            expect(window.updateResult).toHaveBeenCalledWith("Operation not recognized");
+            // calledtimes takes an arg for how many times it should run
+            expect(window.updateResult).toHaveBeenCalledTimes(1)
         });
         it("validates expression when the second number is invalid", function(){
-            spyOn(window, "updateResult").and.stub();
+            spyOn(window, "updateResult")
 
             calculate("3+a");
             // our spy was installed on window.updateResult so we have to expect that
             // checking to see if this has been called at least once when calculate has been executed
             expect(window.updateResult).toHaveBeenCalled()
+            expect(window.updateResult).toHaveBeenCalledWith("Operation not recognized");
+            expect(window.updateResult).toHaveBeenCalledTimes(1)
         });
         it("validates expression when operation is invalid", function(){
-            spyOn(window, "updateResult").and.stub();
+            spyOn(window, "updateResult")
 
             calculate("3_4");
             // our spy was installed on window.updateResult so we have to expect that
             // checking to see if this has been called at least once when calculate has been executed
             expect(window.updateResult).toHaveBeenCalled()
+            expect(window.updateResult).toHaveBeenCalledWith("Operation not recognized");
+            expect(window.updateResult).toHaveBeenCalledTimes(1)
         });
         xit("calls add");
         xit("calls subtract");
